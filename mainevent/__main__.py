363c5002ca1eb7dac53b01174b16d18cacd2a2a4
Liam <learley1@babson.edu>
2025-04-01 22:11:59 -0400

    Modifications

--- mainevent/__main__.py
+++ mainevent/__main__.py
@@ -15,7 +15,7 @@ from mainevent.mainevent import subscriber, publisher, Pool
 
 
 if __name__ == '__main__':
-    @subscriber("example", host='0.0.0.0', port=9019)
+    @subscriber("test", host='0.0.0.0', port=9019)
     async def print_egads(event):
         #print(f"Edit ID: {event}")
         return (event.data)
--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='mainevent_client',
     version='0.1',
     license='MIT',
-    url='https://github.com/mypackage.git',
     author='Liam Earley',
     author_email='liamearley7@gmail.com',
     description='Mainevent SSE Server Python Client',
