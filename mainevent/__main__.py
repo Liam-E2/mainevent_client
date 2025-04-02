02692b5b5d1f63a060ea4f1cfce1fed342aada0e
Liam <learley1@babson.edu>
2025-04-01 22:36:15 -0400

    package dir

--- mainevent/__main__.py
+++ mainevent/__main__.py
@@ -15,6 +15,7 @@ from mainevent.mainevent import subscriber, publisher, Pool
 
 
 if __name__ == '__main__':
+    print("Running")
     @subscriber("test", host='0.0.0.0', port=9019)
     async def print_egads(event):
         #print(f"Edit ID: {event}")
--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author='Liam Earley',
     author_email='liamearley7@gmail.com',
     description='Mainevent SSE Server Python Client',
+    package_dir = {"": "mainevent"},
     packages=find_packages(),    
     install_requires=['wheel', 'aiohttp', 'aiosseclient', 'aiostream'],
 )
